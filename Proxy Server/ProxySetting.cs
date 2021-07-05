00dbbf7062e978521353771a78f9b207570a7770
Nguyễn Đăng Thánh <dangthanh_252@hotmail.com>
2021-07-05 21:29:03 +0700

    Update code setting

---
+++ Proxy Server/ProxySetting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy_Server
+{
+    public class ProxySetting
+    {
+        public string ProxyServer { get; set; }
+        public string DomainList { get; set; }
+    }
+}
