0a52ea8a68d195f4204e8dc4ac6d60f3b2fda729
Nguyễn Đăng Thánh <dangthanh_252@hotmail.com>
2021-07-30 14:28:44 +0700

    Add API

---
+++ StrongProxyAPI/StrongProxyAPI/Constant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StrongProxyAPI
+{
+    public class Constant
+    {
+        public const string SETTING_PATH = @"C:\StrongProxy\ProxySetting.csv";
+    }
+}
---
+++ StrongProxyAPI/StrongProxyAPI/Controllers/ProxySettingController.cs
@@ -0,0 +1,43 @@
+﻿using CsvHelper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StrongProxyAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+
+namespace StrongProxyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProxySettingController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<ProxySetting> Get()
+        {
+            ProxySetting proxySetting = ReadSetting();
+            return proxySetting;
+        }
+
+        private ProxySetting ReadSetting()
+        {
+            ProxySetting setting = null;
+            string file = Constant.SETTING_PATH;
+            if (System.IO.File.Exists(file))
+            {
+                using (StreamReader reader = new StreamReader(file))
+                using (CsvReader csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    IEnumerable<ProxySetting> records = csv.GetRecords<ProxySetting>();
+
+                    setting = records.FirstOrDefault();
+                }
+            }
+            return setting;
+        }
+    }
+}
